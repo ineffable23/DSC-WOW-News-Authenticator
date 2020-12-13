a50b562372fff8aaee4819e7bf92f2056f437c73
Harshita Verma <49369387+ineffable23@users.noreply.github.com>
2020-12-13 11:48:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,7 @@ def hello_world():
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
-    # int_features=[int(x) for x in request.form.values()]
-    # final=[np.array(int_features)]
-    # print(int_features)
-    # print(final)
+    
     prediction=pac.predict(tfidf_test)
     cf_matrix = confusion_matrix(y_test, y_pred, labels=['FAKE', 'REAL'])
     print(cf_matrix)
@@ -26,12 +23,6 @@ def predict():
 
     output = sns.heatmap(cf_matrix / np.sum(cf_matrix), annot=True,
                 fmt='.2%', cmap='Blues')
-
-
-    # if output>str(0.5):
-    #     return render_template('forest_fire.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
-    # else:
-    #     return render_template('forest_fire.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
 
 
 if __name__ == '__main__':
